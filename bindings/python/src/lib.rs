1074954c693c8b767a7b7ee968ec64a1157f005a
Walter Sperat <walter@sperat.com.ar>
2025-09-05 01:17:06 +0200

    update toml file contents and repository structure

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 
 #[pymodule]
-fn forestfire_python(_py: Python, m: &PyModule) -> PyResult<()> {
+fn forestfire(_py: Python, m: &PyModule) -> PyResult<()> {
     // add functions/classes to `m` later
     Ok(())
 }
