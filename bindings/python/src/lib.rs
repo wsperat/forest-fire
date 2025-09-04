2ddfd8de752411851ddc8d820f69ac692d53a3fd
Walter Sperat <walter@sperat.com.ar>
2025-09-05 01:31:01 +0200

    correct pymodule macros

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 
 #[pymodule]
-fn forestfire(_py: Python, m: &PyModule) -> PyResult<()> {
+fn forestfire(_m: &Bound<'_, PyModule>) -> PyResult<()> {
     // add functions/classes to `m` later
     Ok(())
 }
